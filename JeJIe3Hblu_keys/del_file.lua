4e267490363bac88f385dfb4989086b478e3147d
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2021-08-31 09:46:57 +0300

    Delete del_file.lua

--- JeJIe3Hblu_keys/del_file.lua
+++
@@ -1,9 +0,0 @@
-nd
-			chat.say("§6удаление логов завершено")
-			chat.say("§6возобноление наблюдения за периметром")
-	elseif command == "далее" and nick == admin_nick then
-		if modem_BKJI then
-			modem.broadcast(tonumber(TekyIIIuu_nopT_Ha_gJI9l_oTnpaBku), "next")
-		else
-			chat.say("§6модем недоступен!")
-		end
